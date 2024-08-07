dce8de780d318300173014f858bb940be0058d09
Bardia Moshiri <fakeshell@bardia.tech>
2024-08-07 01:57:03 +0000

    debian: build for any and switch to dh compat

--- meson_post_install.sh
+++ meson_post_install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# this should not run for debian packaging, debian takes care of glib schemas with its own hooks
+[ ! -z ${DEB_HOST_MULTIARCH} ] && exit 0
+
+echo "Compiling glib schemas and updating desktop file database"
 glib-compile-schemas $MESON_INSTALL_PREFIX/share/glib-2.0/schemas
 update-desktop-database -q
